382ce239c06a4674bffb46fc2d74800b179da295
Kid <49398015+Kid0h@users.noreply.github.com>
2022-07-16 00:45:26 +0300

    Added support for different types of clients

--- src/main.cpp
+++ src/main.cpp
@@ -12,39 +12,41 @@
 
 #include "font_module.h"
 
-#define DEBUG_LOG(x) if (debug) { x; }
+#define DEBUG_LOG(x) if (debug) { x; std::flush(std::cout); }
 
 void usage();
+bool find_discord_core(std::filesystem::path& discord_path);
+const char* type_type_to_name(const char* client);
 
 int main(int argc, char* argv[]) {
     bool debug = false;
     bool reset = false;
+    std::string client_name;
+    const char* client = nullptr;
     const char* font = nullptr;
 
     // Arguments
     if (argc < 2) { usage(); return -1; }
     for (int i = 1; i < argc; ++i) {
-        // Help
-        if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) { usage(); return 0; }
+        // Client
+        if (strcmp(argv[i], "--client") == 0 || strcmp(argv[i], "-c") == 0) {
+            if ((i == (argc - 1)) || (i == (argc - 2))) { usage(); return -1; }
+            client = argv[++i];
+        }
         // Reset
         else if (strcmp(argv[i], "--reset") == 0 || strcmp(argv[i], "-r") == 0) {
             reset = true;
         }
         // Debug
         else if (strcmp(argv[i], "--debug") == 0 || strcmp(argv[i], "-d") == 0) {
-            if (&argv[i] == &argv[1]) { usage(); return -1; }
+            if (i == argc) { usage(); return -1; }
             debug = true;
         }
-        // Font
-        else if (i == 1) {
-            font = argv[i];
-        }
-        // Usage
+        // Help / Usage
+        else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) { usage(); return 0; }
+        else if (i == (argc - 1)) { font = argv[i]; }
         else { usage(); return -1; }
     }
-    // if (!reset) {
-    //     font = argv[1];
-    // }
 
     /* Get neccesarry paths */
     bool discord_found = false;
@@ -52,32 +54,63 @@ int main(int argc, char* argv[]) {
     std::error_code ec;
     std::filesystem::path module_base, module_path, index_path;
     #ifdef WIN32
-    module_base = std::filesystem::temp_directory_path(ec).parent_path().parent_path() /= "Discord";
+    module_base = std::filesystem::temp_directory_path(ec).parent_path().parent_path();
     #elif __linux__
     struct passwd *pw = getpwuid(getuid());
-    module_base = std::string(pw->pw_dir) + "/.config/discord";
+    module_base = std::string(pw->pw_dir) + "/.config";
     #endif
 
-    // Discord path
-    DEBUG_LOG(std::cout << "Searching Discord's path..");
-    if (!std::filesystem::exists(module_base, ec)) {  }
-    for (auto& dir : std::filesystem::directory_iterator(module_base, ec)) {
-        #ifdef WIN32
-        if (dir.is_directory() && dir.path().filename().wstring().rfind(L"app-1", 0) == 0) {
-        #elif __linux__
-        if (dir.is_directory() && dir.path().filename().wstring().rfind(L"0.", 0) == 0) {
-        #endif
+    // Discord client
+    client_name = type_type_to_name(client);
+    if (client_name.length() == 0) {
+        std::cout << "ERROR: Invalid client type.\n";
+        usage();
+        return -1;
+    }
+    DEBUG_LOG(std::cout << "Searching for " << client_name << "..");
+    for (uint16_t i = 0; i < 3; ++i) {
+        std::string name;
+        std::filesystem::path base = module_base;
+        bool force = client;
+
+        if ((force && (strcmp(client, "stable") == 0)) || (!force && i == 0)) { // Discord
+            if (!force) { name = type_type_to_name("stable"); }
+            #ifdef WIN32
+            base /= "Discord";
+            #elif __linux__
+            base /= "discord";
+            #endif
+        } else if ((force && (strcmp(client, "canary") == 0)) || (!force && i == 1)) { // Discord Canary
+            if (!force) { name = type_type_to_name("canary"); }
+            #ifdef WIN32
+            base /= "DiscordCanary";
+            #elif __linux__
+            base /= "discordcanary";
+            #endif
+        } else if ((force && (strcmp(client, "ptb") == 0)) || (!force && i == 2)) { // Discord PTB
+            if (!force) { name = type_type_to_name("ptb"); }
+            #ifdef WIN32
+            base /= "DiscordPTB";
+            #elif __linux__
+            base /= "discordptb";
+            #endif
+        }
+
+        if (std::filesystem::exists(base) && find_discord_core(base)) {
             discord_found = true;
-            module_base = dir.path();
+            module_base = base;
+            module_base /= "modules";
+            if (!force) { client_name = name; }
+            break;
+        } else if (force) { // Force specific client
             break;
         }
     }
     if (discord_found) {
-        DEBUG_LOG(std::cout << " found!" << std::endl);
+        DEBUG_LOG(std::cout << " found " << client_name << "!" << std::endl);
         // Module directory
-        module_base /= "modules";
     } else {
-        DEBUG_LOG(std::cout << " not found, is Discord installed?" << std::endl) else { std::cout << "Could not find Discord's path" << std::endl; }
+        DEBUG_LOG(std::cout << " not found, is " << client_name << " installed?" << std::endl) else { std::cout << "Could not find " << client_name << ", is it installed?" << std::endl; }
         return -1;
     }
 
@@ -226,9 +259,43 @@ int main(int argc, char* argv[]) {
 
 void usage() {
     std::cout << "Usage: DiscordFontChanger.exe"
-    << " [FONT] [OPTION]" << "\n"
+    << " [options] <font>" << "\n"
+    << "  -c, --client      Target a specific Discord client (stable, canary, ptb)\n"
     << "  -r, --reset       Reset Discord's font to the default\n"
     << "  -d, --debug       Enable debug messages\n"
     << "  -h, --help        Disaply this help message"
     << std::endl;
+}
+
+bool find_discord_core(std::filesystem::path& discord_path) {
+    std::error_code ec;
+    bool found = false;
+
+    for (auto& dir : std::filesystem::directory_iterator(discord_path, ec)) {
+        #ifdef WIN32
+        if (dir.is_directory() && dir.path().filename().wstring().rfind(L"app-1", 0) == 0) {
+        #elif __linux__
+        if (dir.is_directory() && dir.path().filename().wstring().rfind(L"0.", 0) == 0) {
+        #endif
+            found = true;
+            discord_path = dir.path();
+            break;
+        }
+    }
+
+    return found;
+}
+
+const char* type_type_to_name(const char* client) {
+    if (!client || strcmp(client, "stable") == 0) {
+        return "Discord";
+    }
+    else if (strcmp(client, "canary") == 0) {
+        return "Discord Canary";
+    }
+    else if (strcmp(client, "ptb") == 0) {
+        return "Discord PTB";
+    }
+
+    return "";
 }
